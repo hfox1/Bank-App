866c1c32fb3fa0a8adeb1bebbdcae79f714a2058
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-09-21 15:45:46 +0100

    2nd transaction unit test written

--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -8,4 +8,14 @@ describe("Transaction", () => {
 		expect(dep1.deposit).toEqual(100);
 		expect(dep1.date).toEqual("9/21/2022");
 	});
+
+	it("a single null argument causes no error in construction", () => {
+		var dep1 = new Transaction(100, null);
+		dep1.date = "9/21/2022";
+		expect(dep1.withdrawal).toEqual(null);
+	});
+
+	it("two null arguments throws error", () => {
+		expect(new Transaction(null, null)).toThrow();
+	});
 });
