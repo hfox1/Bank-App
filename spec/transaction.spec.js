a65e96b5a1bec47274d13e7ddfd898a443049b79
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-09-21 15:34:09 +0100

    first transaction class unit test written

--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -5,7 +5,7 @@ describe("Transaction", () => {
 	it("a deposit transaction object is created and the amount is checked", () => {
 		var dep1 = new Transaction(100, 0);
 		dep1.date = "9/21/2022";
-		expect(dep1.amount).toEqual(100);
+		expect(dep1.deposit).toEqual(100);
 		expect(dep1.date).toEqual("9/21/2022");
 		done();
 	});
