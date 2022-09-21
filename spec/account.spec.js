d701595a7416c5fa67a93fe96769d899ac1d7be3
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-09-21 16:28:57 +0100

    add transaction to acc test written

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -1,4 +1,13 @@
+// Tests
+// 1. initially, there should be an account, with balance 0
+// 2. after an deposit of 100, there should be 100 in the account
+// 3. after one transaction i can print a statement and it shows the date of the transaction, the money moving and balance
+// 4. i can withdraw 50 and it should show a balance of 50
+// 5. after 2 transactions i can print a statement showing both transactions and balances
+// 6.
+
 const Account = require("../lib/account");
+const Transaction = require("../lib/transaction");
 
 describe("Account", () => {
 	// beforeEach((done) => {
@@ -7,5 +16,14 @@ describe("Account", () => {
 	it("an account has balance 0 on creation", () => {
 		var acc = new Account();
 		expect(acc.balance).toBe(0);
+	});
+
+	describe("working with transactions", () => {
+		it("a transaction can be added to an account", () => {
+			var acc = new Account();
+			var dep1 = new Transaction(100, 0);
+			acc.add(dep1);
+			expect(acc.balance).toBe(100);
+		});
 	});
 });
